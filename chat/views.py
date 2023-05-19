a8c54663eda5a50c5af5875703e9c1f2b989e995
PaolaCartala <ncartala@applaudostudios.com>
2023-05-19 19:16:56 -0300

    Add gitignore and fix views and settings

--- chat/views.py
+++ chat/views.py
@@ -7,24 +7,37 @@ from chatgpt_integration.settings import API_KEY
 
 # Create your views here.
 class ChatView(View):
-    template_name = 'chat.html'
+    """
+    View for handling chat interactions with ChatGPT.
+    """
+    template_name: str = 'chat.html'
 
-    def get(self, request):
+    def get(self, request) -> render:
+        """
+        Handle GET request and render the chat template.
+        """
         return render(request, self.template_name)
 
-    def post(self, request):
+    def post(self, request) -> render:
+        """
+        Handle POST request with user's question and generate chatbot response.
+        """
         question = request.POST['question']
         response = self.get_chatbot_response(question)
         return render(request, self.template_name, {'response': response})
 
-    def get_chatbot_response(self, question):
+    def get_chatbot_response(self, question: str) -> str:
+        """
+        Get the response from the ChatGPT API for the given question.
+        """
         api_key = API_KEY
-        url = 'https://api.openai.com/v1/engines/davinci-codex/completions'
+        url = 'https://api.openai.com/v1/completions'
         headers = {
             'Content-Type': 'application/json',
             'Authorization': f'Bearer {api_key}'
         }
         data = {
+            'model': "text-davinci-003",
             'prompt': question,
             'max_tokens': 50
         }
